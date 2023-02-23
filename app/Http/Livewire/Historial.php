2324137a12490d80ef0455315f59c3b047c4ed88
juan cornejo <juanramoncornejo@gmail.com>
2023-02-23 17:19:45 -0300

    se corrige historial vista y controlador agragando consulta y foreache en vista

--- app/Http/Livewire/Historial.php
+++ app/Http/Livewire/Historial.php
@@ -7,21 +7,21 @@ use Livewire\Component;
 
 class Historial extends Component
 {
+    public $historicos;
 
     public function mount($fechas_id)
 
     {
 
-            $this->historico = Resultado::where('fechas_id' ,$fechas_id)->orderby('id','Desc')->first();
-            //  dd($this->historico);
+            $this->dato = Resultado::where('fechas_id' ,$fechas_id)->orderby('id','Desc')->first();
 
-       
-
+            $this->historicos = Resultado::where('fechas_id' ,$fechas_id)->orderby('id','Desc')->get();
 
     }
 
     public function render()
     {
+
 
         return view('livewire.historial');
     }
--- resources/views/livewire/historial.blade.php
+++ resources/views/livewire/historial.blade.php
@@ -4,14 +4,16 @@
 
     <div class="container mx-auto mt-5 mb-5">
         <div class="text-center uppercase font-bold text-gray-600 px-4 py-2 bg-white w-4/5 mx-auto rounded-lg shadow-xl">Resultados Fecha
-            {{$this->historico->fechas_id}}
+            {{$this->dato->fechas_id}}
         </div>
     </div>
 
     <div class="container mx-auto mt-5 mb-4">
 
             <div class="text-center text-gray-600 px-4 py-2  ml-2 mr-2 mx-auto  space-y-6">
+                @foreach ( $historicos as $historico )
                 <div class="bg-white  ml-2 mr-2 mx-auto rounded-lg py-4 mb-2">
+
                     <div class="mb-2 text-md uppercase text-gray-600 font-bold">{{ $historico->serie->nombre}} {{ $historico->tipos->tipo}}</div>
                     <div class="flex flex-rows justify-around ">
                         <div class="py-1"><img src="{{ Storage::url( $historico->log->logo)}}" class="h-10"></div>
@@ -19,11 +21,17 @@
                         <div class="py-2 text-md text-gray-600 font-bold text-xl">-</div>
                         <div class="py-2 text-md text-gray-600 font-bold text-xl">{{ $historico->resultado_visita}}</div>
                         <div><img src="{{ Storage::url($historico->logo->logo)}}" class="h-10"></div>
+
                     </div>
                     <div class="text-sm text-center text-md rounded-lg bg-gray-800 ml-4 mr-4 mt-5 text-white font-bold">{{ $historico->estado->status}}</div>
                 </div>
+                @endforeach
             </div>
     </div>
+
+
+
+
     @include('menu')
 </div>
 
